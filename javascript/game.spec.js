b3b16f613081cd7b74bca11c3c9ca10b89a867bf
bens <bens@spreadshirt.net>
2017-11-20 17:39:49 +0100

    style: autoformat js files

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -1,17 +1,17 @@
 require('./game.js');
 
 describe("The test environment", function() {
-  it("should pass", function() {
-    expect(true).toBe(true);
-  });
+	it("should pass", function() {
+		expect(true).toBe(true);
+	});
 
-  it("should access game", function() {
-  	// console.log(Game);
-    expect(Game).toBeDefined();
-  });
+	it("should access game", function() {
+		// console.log(Game);
+		expect(Game).toBeDefined();
+	});
 });
 
 describe("Your specs...", function() {
 	// run with App().start()
-  // it ...
+	// it ...
 });
